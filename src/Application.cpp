f5f58cba3089a033e9a9e1cd30cfdf6fcea53c7b
Ayan Majumdar <majumdarayan05@gmail.com>
2025-02-02 15:22:17 +0530

    Change OpenGL Profile from COMPAT to CORE and Implement Vertex Array Objects

--- src/Application.cpp
+++ src/Application.cpp
@@ -118,6 +118,11 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3); // OpenGl Major version 3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // OpenGl Minor version 3
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //Core Profile
+
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(540, 540, "OPENGL-1", NULL, NULL);
     if (!window)
@@ -151,22 +156,29 @@ int main(void)
         0,1,2,
         2,3,0
     };
+
+    //Vertex Array Object
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao)); //Bind VAO 
     //send VERTEX BUFFER DATA TO GPU
     unsigned int buffer;   
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6* 2* sizeof(float), positions, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &buffer));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 4* 2* sizeof(float), positions, GL_STATIC_DRAW));
 
-    glEnableVertexAttribArray(0);
+
+    //Specify format of Vertex Data
+    GLCall(glEnableVertexAttribArray(0));
   //glVertexAttribPointer(index,size,type     ,normalized,stride            ,pointer)
-    glVertexAttribPointer(0    ,2   , GL_FLOAT, GL_FALSE , sizeof(float) * 2, 0);
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));//layout of data BINDS VERTEX BUFFER TO VAO
 
 
     //send INDEX BUFFER DATA TO GPU
     unsigned int ibo; // IndexBufferObject
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &ibo));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
     
     //Creating shader
     
@@ -182,7 +194,7 @@ int main(void)
 
     //BIND SHADER
     unsigned int shader = createShader(source.VertexSource, source.FragmentSource);
-    glUseProgram(shader);
+    GLCall(glUseProgram(shader));
 
     //Retrieving location of Uniform in shader Program
 
@@ -197,16 +209,33 @@ int main(void)
     float g_increment = 0.00f;
     float b_increment = 0.00f;
 
-    
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+
+        GLCall(glUseProgram(shader));
         GLCall(glUniform4f(location, r, g, b, 1.0f));
+        /*
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+        
+        glEnableVertexAttribArray(0);
+        
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+        */
+		GLCall(glBindVertexArray(vao));
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+        
         //glDrawArrays(GL_TRIANGLES,0, 6);
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr)); //Draw Call
         /*
         if (r > 1.0f ) {
             r_increment = -0.05f;
